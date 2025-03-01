4dc39686c8b334e0f5c824afec7493dbe140aa35
Will FP <william.favierparsons1@gmail.com>
2025-03-01 10:13:01 +0000

    Optimised dataset to load full data into RAM first

--- modeling/dataset.py
+++ modeling/dataset.py
@@ -3,18 +3,14 @@ import torch
 
 
 class ChessDataset(torch.utils.data.Dataset):
-    def __init__(self, h5_path):
-        self.h5_path = h5_path
-
-        with h5py.File(h5_path, 'r') as f:
-            self.length = len(f['boards'])
+    def __init__(self, data_path):
+        with h5py.File(data_path, 'r') as f:
+            self.boards = torch.tensor(f['boards'][:], dtype=torch.float32)
+            self.metadata = torch.tensor(f['metadata'][:], dtype=torch.float32)
+            self.evaluations = torch.tensor(f['evaluations'][:], dtype=torch.float32)
 
     def __len__(self):
-        return self.length
+        return len(self.evaluations)
 
     def __getitem__(self, idx):
-        with h5py.File(self.h5_path, 'r') as f:
-            board = torch.FloatTensor(f['boards'][idx]).permute(2, 0, 1)
-            metadata = torch.FloatTensor(f['metadata'][idx])
-            evaluation = torch.FloatTensor([f['evaluations'][idx]]).squeeze()
-            return board, metadata, evaluation
+        return self.boards[idx].permute(2, 0, 1), self.metadata[idx], self.evaluations[idx].squeeze()
